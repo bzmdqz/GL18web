cb8ee9832a3b3bf2fc131c452266d7ff9d242078
bzmdqz <114287249+bzmdqz@users.noreply.github.com>
2023-02-11 10:31:46 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,107 +1,74 @@
-window.addEventListener('load', function () {
-    var arrow_l = document.querySelector(".arrow-l");
-    var arrow_r = document.querySelector(".arrow-r");
-    var focus = document.querySelector(".focus");
-    var ul = focus.querySelector('ul');
-    var ol = focus.querySelector('ol');
-    var focusWidth = focus.offsetWidth;
- 
-    focus.addEventListener("mouseenter", function(){
-        arrow_l.style.display = 'block';
-        arrow_r.style.display = 'block';
-        // 自动停止定时轮播
-        clearInterval(timer);
-        timer = null;
-    })
-    focus.addEventListener("mouseleave", function(){
-        arrow_l.style.display = 'none';
-        arrow_r.style.display = 'none';
-        // 自动重启定时轮播
-        timer = setInterval(function () {
-            arrow_r.click();
-        }, 2000)
-    })
- 
-    for(var i = 0; i < ul.children.length; i++){
-        // 动态创建li
-        var li = document.createElement("li");
-        // 1、li添加索引号index
-        li.setAttribute('index',i);
-        // 将li添加到ol
-        ol.appendChild(li);
- 
-        // 2、点击的圆圈高亮，排他思想
-        li.addEventListener('click', function(){
-            for(var i = 0; i < ol.children.length; i++){
-                ol.children[i].className = '';
-            }
-            this.className = 'current';
-            // 3、点击圆圈得到对应的index
-            var index = this.getAttribute('index');
-            // num、index 赋值的作用是点击圆圈的同时改变num和index的默认值，防止出现bug
-            num = index;
-            circle = index;
-            // console.log(index);
-            // console.log(focusWidth);
-            // console.log(-index * focusWidth);
- 
-            // 4、点击圆圈图片滚动
-            animate(ul, -index * focusWidth);
-            // 遇到的bug：index和focusWidth数值确实有，animate函数也没问题，但是动画没有滑动
-            // 原因是animate的obj参数中没有使用绝对定位
-        })
-    }
-    // 圆圈默认第一个
-    ol.children[0].className = 'current';
- 
-    // 5、点击左右侧按钮图片无缝滚动 
-    var num = 0;
-    var circle = 0;
-    var firstImg = ul.children[0].cloneNode(true); //深克隆（true）第一张图片，并添加至最后
-    ul.appendChild(firstImg);
-    // 右侧按钮
-    arrow_r.addEventListener('click',function(){
-        if(num == ul.children.length - 1){
-            // 走到最后一张图片是迅速定位到第一张图片
-            ul.style.left = 0;
-            num = 0; //要重置为0
-        }
-        num++;
-        animate(ul, -num * focusWidth);
- 
-        // 点击按钮后圆圈跟着变化
-        circle++;
-        if(circle == ol.children.length){
-            circle = 0; //要重置为0
-        }
-        for(var i = 0; i < ol.children.length; i++){
-            ol.children[i].className = '';
-        }
-        ol.children[circle].className = 'current'
-    })
-    // 左侧按钮
-    arrow_l.addEventListener('click',function(){
-        if(num == 0){       
-            // 走到第一张图片是迅速定位到最后一张图片
-            ul.style.left = -(ul.children.length - 1) * focusWidth + 'px';
-            num = ul.children.length - 1; //要重置为0
-        }
-        num--;
-        animate(ul, -num * focusWidth);
- 
-        // 点击按钮后圆圈跟着变化
-        circle--;
-        if(circle < 0){
-            circle = ol.children.length - 1; //要重置为0
-        }
-        for(var i = 0; i < ol.children.length; i++){
-            ol.children[i].className = '';
-        }
-        ol.children[circle].className = 'current'
-    })
- 
-    // 自动播放，加一个定时器
-    var timer = setInterval(function () {
-        arrow_r.click();
-    }, 2000)
-})
+window.onload=function(){
+var items=document.getElementsByClassName("item");
+var circles=document.getElementsByClassName("circle");
+var leftBtn=document.getElementById("btn-left");
+var rightBtn=document.getElementById("btn-right");
+var content=document.querySelector('.content');
+var index=0;
+var timer=null;
+
+//清除class
+var clearclass=function(){
+	for(let i=0;i<items.length;i++){
+		items[i].className="item";
+		circles[i].className="circle";
+		circles[i].setAttribute("num",i);
+	}
+}
+/*只显示一个class*/
+function move(){
+	clearclass();
+	items[index].className="item active";
+	circles[index].className="circle white";
+}
+//点击右边按钮切换下一张图片
+rightBtn.onclick=function(){
+	if(index<items.length-1){
+		index++;
+	}
+	else{
+		index=0;
+	}
+	move();
+}
+//点击左边按钮切换上一张图片
+leftBtn.onclick=function(){
+	if(index<items.length){
+		index--;
+	}
+	else{
+		index=items.length-1;
+	}
+	move();
+}
+//开始定时器，点击右边按钮，实现轮播
+timer=setInterval(function(){
+	rightBtn.onclick();
+},1500)
+//点击圆点时，跳转到对应图片
+for(var i=0;i<circles.length;i++){
+	circles[i].addEventListener("click",function(){
+		var point_index=this.getAttribute("num");
+		index=point_index;
+		move();
+	})
+
+}
+//鼠标移入清除定时器，并开启一个三秒的定时器，使慢慢转动
+content.onmouseover=function(){
+	clearInterval(timer);
+		timer=setInterval(function(){
+			rightBtn.onclick();
+		},3000)
+}
+//鼠标移出又开启定时器
+content.onmouseleave=function(){
+	clearInterval(timer);
+	timer=setInterval(function(){
+		rightBtn.onclick();
+	},1500)
+}
+}
+————————————————
+版权声明：本文为CSDN博主「念你那丝微笑」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/pig_pig32/article/details/107993759
